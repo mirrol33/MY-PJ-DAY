7f0cb356e6710c47111e46fdc33b399c2ffd4be7
LMK <mirrol33@gmail.com>
2025-05-04 02:15:05 +0900

    05.04 update3

--- my-blog-app/next.config.ts
+++ my-blog-app/next.config.ts
@@ -3,6 +3,7 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   output: 'export',
   basePath: isProd ? '/MY-PJ-DAY' : '',
+  assetPrefix: isProd ? '/MY-PJ-DAY/' : '',
   trailingSlash: true,
 };
 
