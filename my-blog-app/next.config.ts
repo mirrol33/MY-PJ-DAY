57e022224d80a9233cfe5b999eccea07bd97735b
LMK <mirrol33@gmail.com>
2025-05-03 22:42:44 +0900

    05.03 update5

--- my-blog-app/next.config.ts
+++ my-blog-app/next.config.ts
@@ -4,9 +4,6 @@ const nextConfig = {
   output: 'export',
   basePath: isProd ? '/MY-PJ-DAY' : '',
   trailingSlash: true,
-  experimental: {
-    appDir: true,
-  },
 };
 
 export default nextConfig;
