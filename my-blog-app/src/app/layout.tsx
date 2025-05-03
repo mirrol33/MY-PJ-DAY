a0513db95cf2585739408e025b1a224afdf264dc
LMK <mirrol33@gmail.com>
2025-05-04 02:55:14 +0900

    05.04 update6

--- my-blog-app/src/app/layout.tsx
+++ my-blog-app/src/app/layout.tsx
@@ -1,11 +1,14 @@
-// my-blog-app/src/app/page.tsx
+// MY-PJ-DAY/my-blog-app/src/app/layout.tsx
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
-import '@/scss/layout.scss';  // SCSS 스타일 추가
+import '@/scss/layout.scss';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
+      <head>
+        <base href="/MY-PJ-DAY/" />
+      </head>
       <body>
         <Header />
         <main>{children}</main>
--- my-blog-app/src/app/page.tsx
+++ my-blog-app/src/app/page.tsx
@@ -1,4 +1,4 @@
-// my-blog-app/src/app/page.tsx
+// MY-PJ-DAY/my-blog-app/src/app/page.tsx
 export default function HomePage() {
   return <h1>홈 페이지입니다</h1>;
 }
