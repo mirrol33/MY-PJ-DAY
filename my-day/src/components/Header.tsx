aa03d808c76da6d34024f88693750c52721c42e9
LMK <mirrol33@gmail.com>
2025-05-26 20:39:06 +0900

    05.26 update2

--- my-day/src/components/Header.tsx
+++ my-day/src/components/Header.tsx
@@ -1,3 +1,4 @@
+// app/components/Header.tsx
 import Link from "next/link";
 import GoogleAuthButton from "./GoogleAuthButton";
 
@@ -5,8 +6,8 @@ export default function Header() {
   return (
     <header className="bg-gray-600">
       <div className="max-w-4xl mx-auto sm:flex md:flex justify-between items-center">
-      <h1 className="text-white text-sm md:text-lg px-4 py-6">
-        <Link href="/">Next.js + Firebase로 만든 CRUD 게시판</Link>
+      <h1 className="text-white text-lg text-center px-4 py-6">
+        <Link href="/">Next.js + Firebase로 만든 미니 블로그</Link>
       </h1>
       <div className="px-4 py-2">
         <GoogleAuthButton />
